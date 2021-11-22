0781decd68cfaab4cd79f8a3cd5274078394cba3
chel_shev <ixec2011@gmail.com>
2021-11-22 22:27:57 +0300

    feature | fix all

--- src/main/java/dev/chel_shev/nelly/configurations/CorsPrivateConfiguration.java
+++ src/main/java/dev/chel_shev/nelly/configurations/CorsPrivateConfiguration.java
@@ -7,20 +7,20 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-//@Configuration
+@Configuration
 public class CorsPrivateConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<CorsFilter> corsFilter() {
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(true);
-//        config.addAllowedOrigin("http://localhost:3000");
-//        config.addAllowedHeader("*");
-//        config.addAllowedMethod("*");
-//        source.registerCorsConfiguration("/**", config);
-//        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
-//        bean.setOrder(0);
-//        return bean;
-//    }
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
 }
--- src/main/java/dev/chel_shev/nelly/controller/HelloWorldController.java
+++ src/main/java/dev/chel_shev/nelly/controller/HelloWorldController.java
@@ -1,19 +1,19 @@
 package dev.chel_shev.nelly.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloWorldController {
 
-    @RequestMapping("protected/dashboard")
+    @GetMapping("protected/dashboard")
     public String secondPage() {
         return "success";
     }
 
-    @RequestMapping("/dashboard")
+    @GetMapping("/dashboard")
     public String firstPage() {
         return "success";
     }
