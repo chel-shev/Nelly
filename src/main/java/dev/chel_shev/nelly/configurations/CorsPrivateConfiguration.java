ff9f8666106c9024cb58e0b2a586202ab4230cf9
chel_shev <ixec2011@gmail.com>
2021-11-22 22:38:01 +0300

    feature | fix all

--- src/main/java/dev/chel_shev/nelly/configurations/CorsPrivateConfiguration.java
+++ src/main/java/dev/chel_shev/nelly/configurations/CorsPrivateConfiguration.java
@@ -15,7 +15,7 @@ public class CorsPrivateConfiguration {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("http://localhost:3000");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
